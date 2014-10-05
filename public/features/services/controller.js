5ffd31656dd6decad9371000727ed3ebd59f797a
unknown <nandankita@gmail.com>
2014-10-05 10:07:14 -0400

    'URL'

---
+++ public/features/services/controller.js
@@ -0,0 +1,20 @@
+﻿function servicesCtl($scope, $http) {
+    
+    $scope.all = function () {
+        $http.get("/databases")
+        .success($scope.showDatabases);
+    }
+
+    $scope.showDatabases = function (response) {
+        $scope.databases = response;
+    };
+	$scope.all();
+	
+	
+	
+	$scope.urlOptions = [
+      {name:'Protein', value:'protein'},
+      {name:'Nucleotide', value:'nucore'},
+    ];
+	$scope.selectVal =$scope.urlOptions;
+}
--- server.js
+++ server.js
@@ -1,25 +1,54 @@
 var express= require('express');
 var mongojs = require('mongojs');
-var db = mongojs("webdev", ["movie"]);
+var db = mongojs("webdev", ["bioinfo"]);
 var app = express();
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-app.listen(port,ipaddress);
+var mongoUrl  = process.env.OPENSHIFT_MONGODB_DB_URL;
+app.listen(port, ipaddress);
 
-app.get('/',function(req,res){
-	res.send('hello world!!!!');
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
+
+app.get('/env', function (req, res) {
+    res.json(process.env);
 });
 
-var alice ={
-	name:"Alice",
+
+app.get("/databases", function (req, res) {
+    db.bioinfo.find(function (err, docs) {
+        res.json(docs);
+    });
+});
+//var ncbi ={
+//    name: "NCBI",
+//    url: "http://www.ncbi.nlm.nih.gov/"
 	
-};
+//};
+//var ddbj = {
+//    name: "DDBJ",
+//    url: "http://www.ddbj.nig.ac.jp/"
 
-db.movie.insert(alice, function(err,data){
- console.log(err);
- console.log(data);
-});
-app.get('/env',function(req,res){
-	res.json(process.env);
-});
+//};
+//var kegg = {
+//    name: "KEGG",
+//    url: "http://www.genome.jp/kegg/"
+
+//};
+//var pdb = {
+//    name: "PDB",
+//    url: "http://www.rcsb.org/pdb/home/home.do"
+
+//};
+//var ebi = {
+//    name: "EBI",
+//    url: "http://www.ebi.ac.uk/"
+
+//};
+
+//var databases = [ncbi, ddbj, kegg, pdb, ebi];
+//db.bioinfo.insert(databases, function(err,data){
+// console.log(err);
+// console.log(data);
+//});
